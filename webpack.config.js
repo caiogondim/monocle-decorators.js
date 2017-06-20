895b401ee0a4ac33bec7410363dddcbda336ede3
Caio Gondim <me@caiogondim.com>
2017-06-19 22:32:21 -0400

    Enable module concatenation plugin in Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const webpack = require('webpack')
 
 module.exports = {
   entry: './src/index.js',
@@ -6,6 +7,9 @@ module.exports = {
     filename: 'monocle-decorators.js',
     path: path.resolve(__dirname, 'dist')
   },
+  plugins: [
+    new webpack.optimize.ModuleConcatenationPlugin()
+  ],
   module: {
     rules: [
       {
